e4ed9bcc17885f1e7ed2a47eff21dbf7a9f1cc6e
ManSnowThe <ManSnowTh3@gmail.com>
2025-11-04 22:58:00 +0500

    Add dotfiles and vscode settings

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+fish_config theme choose "Rosé Pine"
+
+set -U fish_prompt_pwd_dir_length 0
+set -U fish_greeting
+
+set -gx EDITOR code
+set -gx VISUAL code
